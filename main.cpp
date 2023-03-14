56733fe96f087d4d10f6f35456f04133a19e028a
Angelica Harmon <49004885+angehar27@users.noreply.github.com>
2023-03-13 21:32:08 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+struct gmusical{//Grupo musical (Lista simple con inserción al inicio)
+
+};
+
+struct persona{// lista personas (Lista doble ordenado por cédula).
+
+};
+
+struct horario{//sublista de horario o disponible (lista simple coninserción al final)
+
+};
+
+struct instrumentos{//Instrumentos (lista circular con inserción al final)
+
+};
+
+struct eventos{//Eventos (Lista simple con inserción al final)
+
+};
+
+struct subInstrumentos{//Sublista de instrumentos solicitados (lista simple con inserción al inicio)
+
+};
+
+struct historial{//Historial de eventos (lista doblemente enlazada y circular)
+
+};
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
